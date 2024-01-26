da3a0c856f4575c9f8621af594f7e82c8c614549
altink7 <kelmendialtin48@gmail.com>
2024-01-26 11:16:07 +0100

    frontend init

--- service/api/HammerService.js
+++ service/api/HammerService.js
@@ -1,20 +1,23 @@
+/**
+ * Gets a Hammer by its id
+ */
 $(document).ready(function () {
     $("#getHammered").click(function () {
         let url = "http://localhost:8080/api/hammer/2";
 
-        let quality = $("#quality").val();
-        let price = $("#preis").val();
-        let bezeichnung = $("#bezeichnung").val();
-
-        console.log("quality: "+ quality, + "preis: " + price,
-            "bezeichnung: " + bezeichnung);
-        
         $.get(url, function (data) {
             try{
-                let dataJson= JSON.stringify(data);
-                console.log(dataJson);
-            } catch {
-                console.log("es konnten keine Daten geholt werden bzw gab es dabei ein fehler")
+                $("#hammerTable").html(
+                    "<table>" +
+                    "<tr>" +
+                    "<td>" + data?.bezeichnung + "</td>" +
+                    "<td>" + data?.preis + "</td>" +
+                    "<td>" + data?.quality + "</td>" +
+                    "</tr> + " +
+                    "</table>");
+                console.log(data);
+            } catch (error) {
+                console.log("es konnten keine Daten geholt werden bzw gab es dabei ein fehler" + error)
             }
         })
     });
